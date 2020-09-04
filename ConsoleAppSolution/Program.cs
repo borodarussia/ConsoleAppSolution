7fa68fa250f66a84c3f297645c18b33067e3f64f
borodarussia <boroda1337russia@gmail.com>
2020-09-04 14:30:00 +0400

    input geom parameters

--- ConsoleAppSolution/Program.cs
+++ ConsoleAppSolution/Program.cs
@@ -3,15 +3,15 @@ using System.Security.Cryptography.X509Certificates;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Globalization;
 
 namespace ConsoleAppSolution
 {
     class TubeChannel
     {
-   
 
 
-        public void Parameters(double diam1, double diam2, double length)
+        public void GeomParameters(double diam1, double diam2, double length)
         {
             _diam1 = diam1;
             _diam2 = diam2;
@@ -19,8 +19,7 @@ namespace ConsoleAppSolution
             _area2 = (double)Math.Pow(diam2 / 1000, 2) / 4 * Math.PI;
             _length = length;
         }
-
-        private double _diam1 = 0, _diam2 = 0, _length = 0, _area1 = 0, _area2 = 0;
+        private double _diam1 = 0, _diam2 = 0, _length = 0, _area1 = 0, _area2 = 0;  
         
         public double tube_diameter_in()
         {
@@ -42,6 +41,7 @@ namespace ConsoleAppSolution
         {
             return _length;
         }
+
         public int input_section
         {
             get => _input_section;
@@ -54,6 +54,8 @@ namespace ConsoleAppSolution
         }
         private int _input_section, _output_section;
 
+        public
+
     }
 
     class Program
@@ -64,11 +66,21 @@ namespace ConsoleAppSolution
             int NumChannel = Convert.ToInt32(Console.ReadLine());
             int NumSection = NumChannel + 1;    //Подсчитывается количество сечений(узлов) в схеме
 
+            //***
+            Console.WriteLine("\n***\n");
+
             Console.WriteLine("Введите количество входных сечений");
             int NumSectionIn = Convert.ToInt32(Console.ReadLine());
 
+            //***
+            Console.WriteLine("\n***\n");
+
+
             Console.WriteLine("Введите количество выходных сечений");
             int NumSectionOut = Convert.ToInt32(Console.ReadLine());
+
+            //***
+            Console.WriteLine("\n***\n");
 
             if (NumSectionIn + NumSectionOut < NumSection)
             {
@@ -91,10 +103,12 @@ namespace ConsoleAppSolution
                 Console.WriteLine("Заданное количество входных и выходных сечений больше или равно общему количеству полученных сечений");
             }
 
-            Console.WriteLine("Заполните номера сечений для каналов");
-
             TubeChannel[] channels = new TubeChannel[NumChannel];
 
+            //***
+            Console.WriteLine("\n***\n");
+
+            Console.WriteLine("Заполните номера сечений для каналов");
             for (int i = 0; i < NumChannel; i++)
             {
                 channels[i] = new TubeChannel();
@@ -102,8 +116,26 @@ namespace ConsoleAppSolution
                 channels[i].input_section = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Введите номер выходного сечения для канала №" + (i + 1));
                 channels[i].output_section = Convert.ToInt32(Console.ReadLine());
-            }       
-            
+                //***
+                Console.WriteLine("\n***\n");
+            }
+
+            //***
+            //Console.WriteLine("\n***\n");
+
+            Console.WriteLine("Заполните диаметры и длину канала");
+            for (int i = 0; i<NumChannel; i++)
+            {
+                Console.WriteLine("Введите диаметр на входе в канал №" + (i + 1));
+                double diameter_in = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите диаметр на выходе из канала №" + (i + 1));
+                double diameter_out = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите длину канала №" + (i + 1));
+                double length_channel = Convert.ToInt32(Console.ReadLine());
+                channels[i].GeomParameters(diameter_in, diameter_out, length_channel);
+                //***
+                Console.WriteLine("\n***\n");
+            }
 
         }
     }
