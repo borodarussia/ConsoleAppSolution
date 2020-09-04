65e29077c3c6d73337c29b444f42dd0565f4ec67
borodarussia <boroda1337russia@gmail.com>
2020-09-04 10:28:25 +0400

    create the easy class of first channel

--- ConsoleAppSolution/Program.cs
+++ ConsoleAppSolution/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConsoleAppSolution
 {
+    class TubeChannel
+    {
+        private double _diam1, _diam2, _length;
+
+        public double tube_diameter_1
+        {
+            get => _diam1;
+            set => _diam1 = value;
+        }
+        public double tube_diameter_2
+        {
+            get => _diam2;
+            set => _diam2 = value;
+        }
+        public double tube_length
+        {
+            get => _length;
+            set => _length = value;
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TubeChannel channel1 = new TubeChannel();
+
+            channel1.tube_diameter_1 = 500;
+            channel1.tube_diameter_2 = 500;
+            channel1.tube_length = 200;
+
+            Console.WriteLine("Check, " + channel1.tube_diameter_1);
+
+            //Console.ReadKey()
+
         }
     }
 }
